c857eb71f649373e4ecdfa1afa1170328acc3a01
DenisLupeiko <100043905+DenisLupeiko@users.noreply.github.com>
2022-12-07 19:26:18 +0200

    виконує 8 завдання

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmite);
+
+function handleSubmite(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return console.log("Будь ласка заповніть всі поля!");
+  }
+
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
 
